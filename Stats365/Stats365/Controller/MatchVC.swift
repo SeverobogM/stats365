758a210b98f3bd3c866560d7b1873e55b8565ea9
Yaroslav Ageenko <54582556+TokMatok@users.noreply.github.com>
2022-11-21 20:33:40 +0300

    add some VC

---
+++ Stats365/Stats365/Controller/MatchVC.swift
@@ -0,0 +1,29 @@
+//
+//  Match ViewController.swift
+//  Stats365
+//
+//  Created by Ярослав Агеенко on 21.11.2022.
+//
+
+import UIKit
+
+class MatchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
