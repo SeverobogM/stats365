2b40a17ba7aa41fc4ba2d250adf390b65aeba528
Demid Starikov <severobog@gmail.com>
2022-11-21 22:14:24 +0500

    Xyu

---
+++ Stats365/Stats365/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stats365
+//
+//  Created by Демид Стариков on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
